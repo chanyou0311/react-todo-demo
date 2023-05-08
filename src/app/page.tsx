23e1291fe543badacb7e331cf34e2df52b8810fb
chanyou <chanyou0311@gmail.com>
2023-05-09 01:22:38 +0900

    [add] static components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,53 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+const FilterableTaskTable = () => {
+  return (
+    <>
+      <h1>タスクの一覧</h1>
+      <SearchBar />
+      <TaskTable />
+    </>
+  );
+};
+const SearchBar = () => {
+  return (
+    <form>
+      <label htmlFor="title">タスク名: </label>
+      <input type="text" name="title" id="title" />
+      <input type="submit" value="検索" />
+    </form>
+  );
+};
+const TaskTable = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th>タスク名</th>
+          <th>ユーザー名</th>
+        </tr>
+      </thead>
+      <tbody>
+        <TaskRow />
+        <TaskRow />
+        <TaskRow />
+      </tbody>
+    </table>
+  );
+};
+const TaskRow = () => {
+  return (
+    <tr>
+      <td>☑</td>
+      <td>鉛筆を買う</td>
+      <td>太郎</td>
+    </tr>
+  );
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <FilterableTaskTable />
     </main>
   );
 }
