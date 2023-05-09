71c3ba838248eaa5d4a941f47868f1347928ef16
chanyou <chanyou0311@gmail.com>
2023-05-09 22:45:07 +0900

    [modify] Change space

--- src/app/components/FilterableTaskTable.tsx
+++ src/app/components/FilterableTaskTable.tsx
@@ -3,16 +3,17 @@ import { useState } from "react";
 import { SearchBar } from "./SearchBar";
 import { TaskTable } from "./TaskTable";
 import { Task } from "../models/task";
+import { Space } from "antd";
 
 export const FilterableTaskTable = ({ tasks }: { tasks: Task[] }) => {
   const [searchTitle, setSearchTitle] = useState("");
   const filteredTasks = tasks.filter((task) => task.title.match(searchTitle));
 
   return (
-    <>
+    <Space direction="vertical">
       <h1>タスクの一覧</h1>
       <SearchBar handleChange={(e) => setSearchTitle(e.target.value)} />
       <TaskTable tasks={filteredTasks} />
-    </>
+    </Space>
   );
 };
